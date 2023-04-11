bce42767e6c0d3803c90287343dbfcd3fe770821
rimakhatun3 <rimamnt46@gmail.com>
2023-04-11 20:10:26 +0600

    added filter by

--- src/Components/AppliedJob/AppliedJob.jsx
+++ src/Components/AppliedJob/AppliedJob.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getShoppingCart } from '../../utilitis/fakaDb';
 import { useLoaderData } from 'react-router-dom';
 import ApplyJob from '../ApplyJob/ApplyJob';
 
 const AppliedJob = () => {
     const CartData= useLoaderData()
+    const [filterValue,setFilterValue] = useState("all")
+    
     let newCart = []
     const cart = getShoppingCart()
     for(const id in cart){
@@ -13,12 +15,36 @@ const AppliedJob = () => {
     if(findData){
         newCart.push(findData)
     }
-    console.log(newCart)
+    // console.log(newCart)
+
 }
-    return (
+
+const isExists =newCart.filter((product)=>{
+    console.log(product)
+    if(filterValue ==='remote'){
+        return product.fulltime
+    }
+    else if(filterValue==='fullTime'){
+        return product.remote
+    }else{
+        product
+    }
+})
+const handleFilterValue=(event)=>{
+setFilterValue(event.target.value)
+}
+return (
         <div>
             <h1 className='text-xl font-bold bg-slate-50 py-14 '>Applied Jobs</h1>
-           <div> {newCart.map(data=><ApplyJob key={data.id} data={data}></ApplyJob>)}</div>
+            <div className='mt-10 text-end'>
+            <select onChange={handleFilterValue} className="select select-bordered w-full max-w-xs">
+  <option >all</option>
+  <option>remote</option>
+  <option>onsite</option>
+</select>
+            </div>
+            
+           <div> {newCart.map(data=><ApplyJob key={data.id} isExists={isExists} data={data}></ApplyJob>)}</div>
         </div>
     );
 };
--- src/Components/ApplyJob/ApplyJob.jsx
+++ src/Components/ApplyJob/ApplyJob.jsx
@@ -3,10 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faLocationPin,faCheck } from '@fortawesome/free-solid-svg-icons'
   import { Link } from 'react-router-dom';
 const ApplyJob = ({data}) => {
-    console.log(data)
+    // console.log(data)
     const {companyLogo,jobTitle,location,salary,remote,fullTime,companyName,id} = data
     return (
         <div>
+            
             <div className='lg:flex justify-between items-center my-10 border rounded-md py-6 px-6'>
                <div className='flex gap-5' >
                <img className='border px-12 rounded-lg bg-slate-200  py-24' src={companyLogo} alt="" />
